c869d0525a3b9849c020a595ff9790bb2b66457c
Olga Guselnikova <olchikguselnikova@gmail.com>
2018-08-10 00:44:27 +0300

    Made everything work by some magic, except for slider

--- src/final_project/final_project.js
+++ src/final_project/final_project.js
@@ -1,1 +1,3 @@
 import './final_project.scss';
+
+baron({root: '.trip_slider', scroller: '.trip_slider_wrapper', bar: '.main__bar', scrollingCls: '_scrolling', draggingCls: '_dragging', direction: 'h', impact: 'scroller'});
